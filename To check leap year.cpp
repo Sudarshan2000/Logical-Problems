3f59fd9ec1815b4da5c1d5db96dd4776764e638e
Sudarshan2000 <43596165+Sudarshan2000@users.noreply.github.com>
2020-04-27 10:48:15 +0530

    Update To check leap year.cpp

--- To check leap year.cpp
+++ To check leap year.cpp
@@ -4,19 +4,14 @@
         int year;
         printf("Enter a year: ");
         scanf("%d", &year);
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                // the year is a leap year if it is divisible by 400.
-                if (year % 400 == 0)
-                    printf("%d is a leap year.", year);
-                else
-                    printf("%d is not a leap year.", year);
-            } else
-                printf("%d is a leap year.", year);
-        } else{
-        	     printf("%d is not a leap year.", year);
-
-		}
+        if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))
+	{
+		printf("It is a leap year");
+	}
+	    else
+	    {
+		    printf("It is not a leap year");
+	    }
 		
         getch();    
         return 0;
